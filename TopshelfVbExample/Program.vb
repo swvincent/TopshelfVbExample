ef0641c6dae09427ed0287bede4a5a1d2cdb8915
Scott W. Vincent <scott@swvincent.com>
2019-07-25 22:44:29 -0400

    Add project files.

---
+++ TopshelfVbExample/Program.vb
@@ -0,0 +1,19 @@
+﻿Imports Topshelf
+
+Module Program
+
+    Sub Main()
+        Dim rc = HostFactory.Run(
+            Sub(x)
+                x.RunAsLocalSystem
+                x.SetDescription("Topshelf Example for Visual Basic")
+                x.SetDisplayName("TopshelfVbExample")
+                x.SetServiceName("TopshelfVbExample")
+                x.Service(Of TownCrier)
+            End Sub)
+
+        Dim exitCode = CInt(Convert.ChangeType(rc, rc.GetTypeCode()))
+        Environment.ExitCode = exitCode
+    End Sub
+
+End Module
---
+++ TopshelfVbExample/TownCrier.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Timers
+Imports Topshelf
+
+Public Class TownCrier
+    Implements ServiceControl
+
+    ReadOnly _timer As Timer
+
+    Public Sub New()
+        _timer = New Timer(1000) With {
+            .AutoReset = True}
+
+        AddHandler _timer.Elapsed, AddressOf OnTimedEvent
+    End Sub
+
+    Private Sub OnTimedEvent(source As Object, e As System.Timers.ElapsedEventArgs)
+        Console.WriteLine($"It is {DateTime.Now} and all is well")
+    End Sub
+
+    Public Function Start(hostControl As HostControl) As Boolean Implements ServiceControl.Start
+        _timer.Start()
+        Return True
+    End Function
+
+    Public Function [Stop](hostControl As HostControl) As Boolean Implements ServiceControl.Stop
+        _timer.Stop()
+        Return True
+    End Function
+End Class
